3efd4f56e8c2a9e432f084f725e104166c81c955
JMustang <eecfredes@gmail.com>
2020-12-07 18:47:56 -0300

    feat:task get by status

--- src/repositories/TaskRepository.js
+++ src/repositories/TaskRepository.js
@@ -1,68 +1,85 @@
-const Database = require("../config/Database")
+const Database = require("../config/Database");
 
 const findAll = async () => {
-    const response = await Database.query(
-        `select * from tasks order by id`
-    )
-    return response.rows
-}
+  const response = await Database.query(`select * from tasks order by id`);
+  return response.rows;
+};
+
+const findAllByStatus = async (status) => {
+  const response = await Database.query(
+    `
+        select * from tasks where status = $1 order by id
+    `,
+    [status]
+  );
+
+  return response.rows;
+};
 
 const findTaskById = async (id) => {
-    const response = await Database.query(
-        `
+  const response = await Database.query(
+    `
     select * from tasks where id = $1
-    `, [
-        id
-    ])
-    return response.rows[0]
-}
+    `,
+    [id]
+  );
+  return response.rows[0];
+};
 
-const save = async ({ title, description, status }) => {
-    const response = await Database.query(`
+const save = async ({ user_id, title, description, status }) => {
+  const response = await Database.query(
+    `
     insert into tasks (
-            title, description, status, created_at, updated_at
+        user_id, title, description, status, created_at, updated_at
         ) values (
-            $1, $2, $3, current_timestamp, current_timestamp
+            $1, $2, $3, $4, current_timestamp, current_timestamp
         ) returning *
-    `, [
-        title, description, status
-    ])
+    `,
+    [user_id, title, description, status]
+  );
 
-    return response.rows[0]
-}
+  return response.rows[0];
+};
 
-const findById = async ( id ) => {
-    const response = await Database.query(
-        `
+const findById = async (id) => {
+  const response = await Database.query(
+    `
     select id from tasks where id = $1
-    `, [
-        id
-    ])
+    `,
+    [id]
+  );
 
-    return response.rows[0]
-
-}
+  return response.rows[0];
+};
 
 const remove = (id) => {
-    Database.query(
-        `
+  Database.query(
+    `
         delete from tasks where id = $1
-        `, [
-            id
-        ])
-}
+        `,
+    [id]
+  );
+};
 
 const update = async ({ id, title, description, status, created_at }) => {
-    const response = await Database.query(
-        `
+  const response = await Database.query(
+    `
         update tasks
         set title=$1, description=$2, status=$3, created_at=$4, updated_at=current_timestamp
         where id = $5 returning *
-        `, [
-            title, description, status, created_at, id
-        ])
+        `,
+    [title, description, status, created_at, id]
+  );
 
-        return response.rows[0]
-}
+  return response.rows[0];
+};
 
-module.exports = { findAll, findTaskById, save, findById, remove, update }
+module.exports = {
+  findAll,
+  findAllByStatus,
+  findTaskById,
+  save,
+  findById,
+  remove,
+  update,
+};
--- src/services/TaskService.js
+++ src/services/TaskService.js
@@ -1,28 +1,46 @@
-const TaskRepository = require("../repositories/TaskRepository")
+const TaskRepository = require("../repositories/TaskRepository");
 
 const index = async () => {
-    return await TaskRepository.findAll()
-}
-
-const show = async (id) => {
-    return await TaskRepository.findTaskById(id)
-}
-
-const store = async ({ title, description, status }) => {
-    return await TaskRepository.save({ title, description, status });
+  return await TaskRepository.findAll();
 };
 
-const existsById = async ( id ) => {
-    const response = await TaskRepository.findById( id )
-    return response ? true : false
-}
+const indexByStatus = async (status) => {
+  return await TaskRepository.findAllByStatus(status);
+};
 
-const destroy = (id =>{
-    TaskRepository.remove(id)
-})
+const show = async (id) => {
+  return await TaskRepository.findTaskById(id);
+};
+
+const store = async ({ user_id, title, description, status }) => {
+  return await TaskRepository.save({ user_id, title, description, status });
+};
+
+const existsById = async (id) => {
+  const response = await TaskRepository.findById(id);
+  return response ? true : false;
+};
+
+const destroy = (id) => {
+  TaskRepository.remove(id);
+};
 
 const update = async ({ id, title, description, status, created_at }) => {
-    return await TaskRepository.update({ id, title, description, status, created_at })
-}
+  return await TaskRepository.update({
+    id,
+    title,
+    description,
+    status,
+    created_at,
+  });
+};
 
-module.exports = { index, show, store, existsById, destroy, update }
+module.exports = {
+  index,
+  indexByStatus,
+  show,
+  store,
+  existsById,
+  destroy,
+  update,
+};
